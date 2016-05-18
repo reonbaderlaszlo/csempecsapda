981cf82ae0752a2c0f3a7167c44d5ca02adf4e2f
Báder László <bader.laszlo22@gmail.com>
2016-05-18 15:43:06 +0200

    Upload2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(function(){
     
+	$('.panel.agreement button').on('click', function() {
+
+        $('.panel.agreement').fadeOut('fast', function() {
+
+            $('.panel.description').fadeIn('fast');
+
+        });
+
+    });
+
+    $('.panel.description button').on('click', function() {
+
+        $('.panel.description').fadeOut('fast', function() {
+
+            $('.panel.game').fadeIn('fast');
+
+        });
+
+    });
+
 });
